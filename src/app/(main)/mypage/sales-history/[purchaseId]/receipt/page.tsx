ded481a99e356b67367931840cc5699486bf310e
Bigben2002 <ben50817@gmail.com>
2026-05-19 03:08:55 +0900

    [fix] 뒤로가기 버튼

--- src/app/(main)/mypage/sales-history/[purchaseId]/receipt/page.tsx
+++ src/app/(main)/mypage/sales-history/[purchaseId]/receipt/page.tsx
@@ -1,12 +1,20 @@
 "use client";
 
 import React from "react";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import TransactionReceipt from "../../../../../../components/transaction-receipt/TransactionReceipt";
 
 export default function SalesReceiptPage() {
   const params = useParams();
+  const router = useRouter();
   const saleId = Number(params?.purchaseId ?? NaN);
   if (Number.isNaN(saleId)) return <div>유효하지 않은 ID</div>;
-  return <TransactionReceipt mode="sale" id={saleId} themeColor="#f97316" />;
+  return (
+    <TransactionReceipt
+      mode="sale"
+      id={saleId}
+      themeColor="#f97316"
+      onBack={() => router.push("/mypage/sales-history")}
+    />
+  );
 }
