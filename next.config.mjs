610fbd2f1c794c856d8e13a67aa817a54dd56184
“DayunLee” <leedayun03417@gmail.com>
2026-05-23 20:11:21 +0900

    feat: PWA 지원 기능 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
+import withPWAInit from "next-pwa";
+
 const apiBaseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
 const nextConfig = {
   reactStrictMode: true,
@@ -17,4 +26,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
